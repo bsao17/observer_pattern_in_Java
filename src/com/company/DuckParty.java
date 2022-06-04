785ad6a38587ccd3bd843b69408028611db5c054
bsao17 <declic62@gmail.com>
2022-06-04 08:42:08 +0200

    instantiate new Ducks

--- src/com/company/DuckParty.java
+++ src/com/company/DuckParty.java
@@ -5,9 +5,9 @@ public class DuckParty {
     public static void main(String[] args) {
         Ducks colvert = new Ducks(2, "Colvert");
         Ducks duckToy = new Ducks(1, "toy");
-        Ducks duckPicture = new Ducks(5, "images");
+        Ducks duckPicture = new Ducks(5, "hologram");
 
-        System.out.println("Ce canard est de la race des " + colvert.duckType + " !");
-        System.out.println("et il y en aura " + colvert.duckNumber + " à la canard partie !");
+        System.out.println("The specie of this duck is " + colvert.duckType + " !");
+        System.out.println("and there will " + colvert.duckNumber + " to the duck party !");
     }
 }
