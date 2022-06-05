e9a279558cd54ebb3ac8f551d054c7a564706596
bsao17 <declic62@gmail.com>
2022-06-05 19:28:18 +0200

    complete refactoring in a new git branch

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,23 +1,7 @@
 package com.company;
 
-import com.company.duckParty.Ducks;
-
 public class Main {
     public static void main(String[] args) {
-        Ducks colvert = new Ducks(2, "Colvert");
-        Ducks duckToy = new Ducks(1, "toy");
-        Ducks duckPicture = new Ducks(5, "hologram");
 
-        System.out.println("The specie of this duck is " + colvert.duckType + " !");
-        System.out.println("and there will " + colvert.duckNumber + " duck at the party !");
-
-        colvert.singing(true);
-        colvert.flying(true);
-
-        duckToy.singing(true);
-        duckToy.flying(false);
-
-        duckPicture.singing(false);
-        duckPicture.flying(false);
     }
 }
